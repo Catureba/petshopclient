f3ef32f8315d979f7b0a64334c4fa778a1ef4bf4
mateus.santana <mateus.santana@e-unicred.com.br>
2024-06-07 09:43:29 -0300

    Config(): configurando Zipkin  e Sleuth

--- src/main/java/com/catusoft/petshopclient/infrastructure/repository/product/ProductRepository.java
+++ src/main/java/com/catusoft/petshopclient/infrastructure/repository/product/ProductRepository.java
@@ -3,6 +3,7 @@ package com.catusoft.petshopclient.infrastructure.repository.product;
 import com.catusoft.petshopclient.api.product.ProductDTO;
 import com.catusoft.petshopclient.infrastructure.dao.product.ProductDAO;
 import com.catusoft.petshopclient.infrastructure.dao.product.ProductEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,13 +11,16 @@ import java.util.List;
 import java.util.Objects;
 
 @Repository
+@RequiredArgsConstructor
 public class ProductRepository {
     @Autowired
     private ProductDAO productDAO;
-    private ProductConverter productConverter;
+    private final ProductConverter productConverter;
     public List<ProductDTO> findAll() {
         var productEntities = productDAO.findAll();
-        return productConverter.toDTO(productEntities);
+        if (Objects.isNull(productEntities)) return null;
+        var productDTOs = productConverter.toDTO(productEntities);
+        return productDTOs;
     }
 
     public ProductDTO findById(Long id) {
